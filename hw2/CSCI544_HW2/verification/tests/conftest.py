0688ca8d6e49113aebe9e2e179e3d7ded5f3280b
Kayvan Shah <kayvan.shah@usc.edu>
2023-09-24 17:49:24 -0700

    reformatting

--- hw2/CSCI544_HW2/verification/tests/conftest.py
+++ hw2/CSCI544_HW2/verification/tests/conftest.py
@@ -2,15 +2,15 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--greedy", action="store", default='./out/greedy.json')
-    parser.addoption("--viterbi", action="store", default='./out/viterbi.json')
-    parser.addoption("--model", action="store", default='./out/hmm.json')
-    parser.addoption("--vocab", action="store", default='./out/vocab.txt')
-    parser.addoption("--dev", action="store", default='./data/dev.json')
-    parser.addoption("--test", action="store", default='./data/test.json')
-    
+    parser.addoption("--greedy", action="store", default="./out/greedy.json")
+    parser.addoption("--viterbi", action="store", default="./out/viterbi.json")
+    parser.addoption("--model", action="store", default="./out/hmm.json")
+    parser.addoption("--vocab", action="store", default="./out/vocab.txt")
+    parser.addoption("--dev", action="store", default="./data/dev.json")
+    parser.addoption("--test", action="store", default="./data/test.json")
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def greedy(request):
     name_value = request.config.option.greedy
     if name_value is None:
@@ -18,7 +18,7 @@ def greedy(request):
     return name_value
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def viterbi(request):
     name_value = request.config.option.viterbi
     if name_value is None:
@@ -26,7 +26,7 @@ def viterbi(request):
     return name_value
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def model(request):
     name_value = request.config.option.model
     if name_value is None:
@@ -34,7 +34,7 @@ def model(request):
     return name_value
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def vocab(request):
     name_value = request.config.option.vocab
     if name_value is None:
@@ -42,7 +42,7 @@ def vocab(request):
     return name_value
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def dev(request):
     name_value = request.config.option.dev
     if name_value is None:
@@ -50,7 +50,7 @@ def dev(request):
     return name_value
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def test(request):
     name_value = request.config.option.test
     if name_value is None:
